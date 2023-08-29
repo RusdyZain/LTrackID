a24824bf7c69b54a5dd55cc64d24b024424a3627
RusdyZain <ryanfajri28@gmail.com>
2023-08-30 02:55:26 +0800

    Proyek1

---
+++ js/scriptDest.js
@@ -0,0 +1,101 @@
+ScrollReveal({
+  mobile: false,
+}
+)
+
+ScrollReveal().reveal('.header', {
+  delay: 500,
+  reset: false,
+  mobile: false,
+});
+
+ScrollReveal().reveal('.showcase-content', {
+  scale: 2,
+  duration: 3000,
+  delay: 500,
+  mobile: false,
+});
+
+ScrollReveal().reveal('.showcase-search', {
+  duration: 1500,
+  delay: 500,
+});
+
+ScrollReveal().reveal('.destinations h2', {
+  reset: true,
+  duration: 1500,
+  delay: 500,
+  origin: 'left',
+  distance: '50px',
+});
+
+ScrollReveal().reveal('.destinations-cards', {
+  duration: 1500,
+});
+
+ScrollReveal().reveal('.section-title', {
+  reset: true,
+  duration: 1500,
+  delay: 500,
+  origin: 'left',
+  distance: '50px',
+});
+
+ScrollReveal().reveal('.hotel-card , #tours, #activities', {
+  duration: 1500,
+  origin: 'left',
+  distance: '50px',
+});
+
+ScrollReveal().reveal('.about-content', {
+  reset: true,
+  duration: 1500,
+  origin: 'left',
+  distance: '50px',
+});
+
+ScrollReveal().reveal('.about-img', {
+  reset: true,
+  duration: 1500,
+  origin: 'right',
+  distance: '50px',
+});
+
+const swiper = new Swiper('.swiper1', {
+  direction: 'horizontal',
+  loop: true,
+  speed: 600,
+  slidesPerView: 6,
+  spaceBetween: 10,
+  breakpoints: {
+    240: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+  },
+});
+
+const hotelButton = document.querySelector('.hotel-button');
+const hotelCard = document.querySelectorAll('.off');
+const text = hotelButton.innerText;
+
+hotelButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  hotelCard.forEach((x) => {
+    x.classList.toggle('on');
+  });
+
+  if (e.target.innerHTML !== 'less <img src="/image/Imgs/icons/bleft.png">') {
+    e.target.innerHTML = 'less <img src="/image/Imgs/icons/bleft.png" >';
+  } else {
+    e.target.innerHTML = 'view all <img src="/image/Imgs/icons/bleft.png" >';
+  }
+});
---
+++ js/scriptHome.js
@@ -0,0 +1,16 @@
+ScrollReveal({
+    mobile: false,
+}
+)
+
+ScrollReveal().reveal('.header', {
+    delay: 500,
+    reset: false,
+    mobile: false,
+});
+
+ScrollReveal().reveal('.wrapper', {
+    scale: 2,
+    duration: 3000,
+    mobile: false,
+  });
